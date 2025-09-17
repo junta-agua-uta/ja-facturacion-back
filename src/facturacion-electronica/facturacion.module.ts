9201878b42b01657c818a8f950f6e626c151ba6a
Steeven24 <loorjins@gmail.com>
2025-09-17 09:21:28 -0500

    Se implemento el modulo de liquidacion de compra

--- src/facturacion-electronica/facturacion.module.ts
+++ src/facturacion-electronica/facturacion.module.ts
@@ -4,16 +4,19 @@ import { GeneratePdfService } from './services/generate-pdf.service'
 import { SignInvoiceService } from './services/sign-invoice.service'
 import { EmailModule } from './email/email.module'
 import { ElectronicInvoiceService } from './services/electronic-invoice.service'
+import { GenerateLiquidacionCompraService } from './services/generate-liquidacion-compra.service'
+import { LiquidacionCompraController } from './controllers/liquidacion-compra.controller'
 
 @Module({
-  controllers: [],
+  controllers: [LiquidacionCompraController],
   providers: [
     GenerateInvoiceService,
     GeneratePdfService,
     SignInvoiceService,
     ElectronicInvoiceService,
+    GenerateLiquidacionCompraService,
   ],
   imports: [EmailModule],
-  exports: [GenerateInvoiceService, ElectronicInvoiceService],
+  exports: [GenerateInvoiceService, ElectronicInvoiceService, GenerateLiquidacionCompraService],
 })
 export class FacturacionModule {}
