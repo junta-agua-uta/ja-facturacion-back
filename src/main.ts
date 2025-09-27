7aa14b9856824722571bc51b684f6b801af6e208
Rafa Soriano <raansopro324@gmail.com>
2025-09-27 01:25:39 -0500

    Update Dockerfile to remove NODE_ENV variable and enhance CORS configuration in main.ts to allow localhost

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
 
   app.enableCors({
-    origin: [process.env.FRONT_ORIGIN],
+    origin: [process.env.FRONT_ORIGIN, 'http://localhost:5173'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
