74078355737e5a2d41c04e9a7df530693afe34bd
Rafa Soriano <raansopro324@gmail.com>
2025-09-27 01:32:08 -0500

    Refactor Dockerfile to streamline dependency installation and update CORS configuration in main.ts to enhance security and maintainability

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
 
   app.enableCors({
-    origin: [process.env.FRONT_ORIGIN, 'http://localhost:5173'],
+    origin: [
+      'https://p01--dev-ja-facturacion-front--hcqk4hv77kmd.code.run',
+      'http://localhost:5173',
+    ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
