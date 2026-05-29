a765480d3ad9315fbc83427bed4438ec8148f520
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2026-05-29 14:30:45 -0500

    fix. user password

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,26 @@ import { RegisterService } from './services/register.service'
 import { AuthMiddleware } from './middleware/auth.middleware'
 import { LoginService } from './services/login.service'
 import { FindUserService } from './services/findUserByCedula.service'
+import { UpdateUserService } from './services/updateUser.service'
 
 @Module({
   imports: [],
   controllers: [AuthController],
-  providers: [RegisterService, LoginService, FindUserService],
+  providers: [
+    RegisterService,
+    LoginService,
+    FindUserService,
+    UpdateUserService,
+  ],
 })
 export class AuthModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(AuthMiddleware)
-      .forRoutes({ path: '/auth/register', method: RequestMethod.POST })
+      .forRoutes(
+        { path: '/auth/change-password', method: RequestMethod.PATCH },
+        { path: '/auth/profile', method: RequestMethod.PATCH },
+        { path: '/auth/register', method: RequestMethod.POST },
+      )
   }
 }
